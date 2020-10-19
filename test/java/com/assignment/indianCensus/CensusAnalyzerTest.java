bd13455b591ca89c3ef4856b98be5dc1291d2d30
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-20 02:05:14 +0530

    Modified:Test case to check the CSV file type is correct or not

--- test/java/com/assignment/indianCensus/CensusAnalyzerTest.java
+++ test/java/com/assignment/indianCensus/CensusAnalyzerTest.java
@@ -9,6 +9,7 @@ public class CensusAnalyzerTest {
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "/Users/saideeksha/Desktop/eclipse-workspace/indianCensus/src/test/resources/IndiaStateCensusData.csv";
 	private static final String INDIA_STATECODE_CSV_FILE_PATH = "/Users/saideeksha/Desktop/eclipse-workspace/indianCensus/src/test/resources/IndiaStateCode.csv";
 	private static final String WRONG_STATECODE_CSV_FILE_PATH = "/Users/saideeksha/Desktop/eclipse-workspace/indianCensus/src/main/resources/IndiaStateCode.csv";
+	private static final String WRONG_STATECODE_CSV_FILE_TYPE = "/Users/saideeksha/Desktop/eclipse-workspace/indianCensus/src/test/resources/IndiaStateCode.java";
 
 	@Test
 	public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -37,6 +38,18 @@ public class CensusAnalyzerTest {
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(CensusAnalyzerException.class);
 			censusAnalyser.loadStateCode(WRONG_STATECODE_CSV_FILE_PATH);
+		} catch (CensusAnalyzerException e) {
+			Assert.assertEquals(CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+		}
+	}
+
+	@Test
+	public void givenIndianStateCodeData_WithWrongFileType_ShouldThrowException() {
+		try {
+			CensusAnalyzer censusAnalyser = new CensusAnalyzer();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyzerException.class);
+			censusAnalyser.loadIndiaCensusData(WRONG_STATECODE_CSV_FILE_TYPE);
 		} catch (CensusAnalyzerException e) {
 			Assert.assertEquals(CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
 		}
